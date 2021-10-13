062ce516cbe5e6c6f58c9e9be1da25b7e7a4356e
MatsudairaSyuMe <scotthong@hotmail.com>
2021-10-13 16:49:32 +0800

    extend glyph buffer for two situation for Prt_Text

--- src/main/java/com/systex/sysgateii/autosvr/prtCmd/Impl/CS4625Impl.java
+++ src/main/java/com/systex/sysgateii/autosvr/prtCmd/Impl/CS4625Impl.java
@@ -494,7 +494,8 @@ public class CS4625Impl implements Printer {
 		int j, offset = 0;
 		boolean bLineFeed;
 		boolean bHalf = false;
-		byte[] hBuf = new byte[600];
+		//20211012 MatsudiaraSyuMe extend size of hBuf from 600 to 1500
+		byte[] hBuf = new byte[1500];
 
 		// filter space 91.10.09
 		bLineFeed = false;
@@ -777,8 +778,8 @@ public class CS4625Impl implements Printer {
 		int j, offset = 0;
 		boolean bLineFeed;
 		boolean bHalf = false;
-		//20211012 MatsudiaraSyuMe extend size of hBuf from 600 to 1280
-		byte[] hBuf = new byte[1280];
+		//20211012 MatsudiaraSyuMe extend size of hBuf from 600 to 1500
+		byte[] hBuf = new byte[1500];
 
 		// filter space 91.10.09
 		bLineFeed = false;
